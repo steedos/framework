8c4e971e326aea125d7cba40f6c78fa12a9a46b1
zhuangjianguo <zhuangjianguo@hotoa.com>
2017-03-25 22:01:57 +0800

    add source code to package

--- packages/meteor-adminlte/package.js
+++ packages/meteor-adminlte/package.js
@@ -1,13 +1,10 @@
 Package.describe({
   name: 'steedos:adminlte',
-  version: '2.3.11_1',
+  version: '2.3.11_2',
   summary: 'adminlte package',
   git: ''
 });
 
-Npm.depends({
-  "admin-lte": "2.3.11"
-});
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.3');
@@ -15,9 +12,11 @@ Package.onUse(function(api) {
   api.use('coffeescript');
   api.use('ecmascript');
 
-  api.use('steedos:bootstrap@3.3.7');
+  api.use('steedos:bootstrap@3.3.7_1');
 
-  api.addFiles('core.js', "client");
+  api.addFiles('js/app.js', "client");
+  api.addFiles('css/AdminLTE.css', "client");
+  api.addFiles('css/skins/skin-blue.css', "client");
 
 });
 
