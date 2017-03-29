cf9b91e98144697b597fe698ce48a96827207378
Jack Zhuang <zhuangjianguo@me.com>
2017-03-29 15:20:10 +0800

    add user accounts package

---
+++ packages/steedos-accounts/package.js
@@ -0,0 +1,41 @@
+Package.describe({
+  name: 'steedos:accounts',
+  version: '0.0.1',
+  summary: 'User Accounts configration for Steedos',
+  git: 'https://github.com/steedos/developer/packages/steedos-accounts'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.3');
+
+  api.use('coffeescript');
+  api.use('ecmascript');
+  api.use('blaze-html-templates');
+  api.use('accounts-password');
+  api.use('underscore');
+
+  api.use('flemay:less-autoprefixer@1.2.0');
+  api.use('kadira:flow-router@2.10.1');
+  api.use('tap:i18n', ['client', 'server']);
+  tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
+  api.addFiles(tapi18nFiles, ['client', 'server']);
+
+  api.use('useraccounts:bootstrap@1.14.2');
+  api.use('useraccounts:core@1.14.2');
+  api.use('useraccounts:flow-routing@1.14.2');
+
+  api.addFiles('lib/core.coffee');
+  api.addFiles('client/router.coffee', "client");
+
+  api.addFiles('client/layout/login_layout.html', "client");
+  api.addFiles('client/layout/login_layout.coffee', "client");
+  api.addFiles('client/layout/login_layout.less', "client");
+
+
+  api.export('Accounts');
+
+});
+
+Package.onTest(function(api) {
+
+});
