0797d580fbd6b7f5d47637ad85930bd9e97094a4
Jack Zhuang <zhuangjianguo@me.com>
2017-03-30 18:53:10 +0800

    去掉无用的 depend on

--- packages/steedos-base/package.js
+++ packages/steedos-base/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'steedos:base',
-  version: '0.0.5',
+  version: '0.0.6',
   summary: 'Steedos libraries',
   git: 'https://github.com/steedos/platform/packages/steedos-theme'
 });
@@ -16,15 +16,7 @@ Package.onUse(function(api) {
   api.use('reactive-var');
   api.use('tracker');
 
-  api.use('dburles:collection-helpers@1.1.0');
   api.use('flemay:less-autoprefixer@1.2.0');
-  api.use('simple:json-routes@2.1.0');
-  api.use('aldeed:simple-schema@1.5.3');
-  api.use('aldeed:collection2@2.10.0');
-  api.use('aldeed:tabular@2.1.1');
-  api.use('aldeed:autoform@5.0.0');
-  api.use('matb33:collection-hooks@0.8.4');
-
   api.use('kadira:flow-router@2.10.1');
   api.use('meteorhacks:subs-manager@1.6.4');
 
